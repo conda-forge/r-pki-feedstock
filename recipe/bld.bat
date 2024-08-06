00aeb099e24931cc0e4e5655f09fd4664bf7dde2
Mervin Fansler <mmfansler@gmail.com>
2024-08-07 00:01:52 +0200

    set paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
-set LOCAL_SOFT=%PREFIX%\Library\mingw-w64
-"%R%" CMD INSTALL --build .
-IF %ERRORLEVEL% NEQ 0 exit 1
+set PKG_CPPFLAGS=-I%PREFIX%\Library\include
+set PKG_LIBS=-L%PREFIX%\Library\lib
+"%R%" CMD INSTALL --build . %R_ARGS%
+IF %ERRORLEVEL% NEQ 0 exit /B 1
